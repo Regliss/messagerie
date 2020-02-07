f6e2a2e965b262a80f35ce3dcbf209e7a41b8dbf
darkregliss <pomart.thomas1@gmail.com>
2020-02-07 15:32:02 +0100

    deuxpointzero

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -1,19 +1,53 @@
 <?php
 
+
+
 namespace App\Controller;
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\User;
+
+use App\Entity\Groupe;
+
+
+
 class MessageController extends AbstractController
+
 {
+
     /**
-     * @Route("/message", name="message")
+
+     * @Route("/message/{id}", name="message")
+
      */
-    public function message()
+
+    public function message($id)
+
     {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+
+        $repoG = $this -> getDoctrine() -> getRepository(Groupe::class);
+
+
+
+        $groupe = $repoG -> find($id);
+
+        return $this->render('message/message.html.twig', [
+
+           "users" => $groupe -> getUsers(),
+
+           "groupe" => $groupe,
+
+           "messages" => $groupe -> getMessages()
+
+            
+
         ]);
+
     }
+
 }
