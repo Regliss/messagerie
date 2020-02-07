e81bb64747101f657ead0e822acd4c5adf582779
Vaenemys <vanraag@gmail.com>
2020-02-07 16:07:53 +0100

    oui

---
+++ src/Controller/GroupeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Groupe;
+use App\Form\GroupeType;
+
+class GroupeController extends AbstractController
+{
+    /**
+    * @Route("/groupeAdd", name="groupeAdd")
+    *
+    */
+public function creationGroupe(Request $request){
+
+    $manager = $this -> getDoctrine() -> getManager();
+    $groupe = new Groupe; 
+
+    $form = $this -> createForm(GroupeType::class, $groupe);
+    $form -> handleRequest($request);
+
+    if($form -> isSubmitted() && $form -> isValid()){
+        $manager -> persist($groupe);
+        
+        $groupe -> setDate(new \DateTime('now'));
+        $groupe -> setUserAdmin($this -> getUser());
+
+        //$groupe -> getUsers() --> array Collection
+        foreach($groupe -> getUsers() as $user){
+            //$groupe -> addUser($user);
+            $user -> addGroupe($groupe);
+        }
+        $manager -> flush();
+    }
+    $this -> render();
+
+}
+
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,17 +6,12 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use App\Entity\User;
-<<<<<<< HEAD
-=======
-use App\Entity\Groupe;
->>>>>>> 20440b84651c124632c69b67b400b1b217749550
 use App\Entity\Message;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-<<<<<<< HEAD
         for($i =1; $i <=3; $i ++){
             $utilisateur = new User;
 
@@ -26,21 +21,11 @@ class AppFixtures extends Fixture
             $utilisateur -> setEmail('user' . $i . '@gmail.com');
             $utilisateur -> setGroupe('Nom du groupe');
             $utilisateur -> setMessages('Nombre de messages');
-=======
-        for($i =1; $i <=10; $i ++){
-            $utilisateur = new User;
-
-            $utilisateur -> setUsername('Utilisateur' . $i);
-            $utilisateur -> setPhoto('Photo'. $i . '.jpg');
-            $utilisateur -> setPassword('123456');
-            $utilisateur -> setEmail('user' . $i . '@gmail.com');
->>>>>>> 20440b84651c124632c69b67b400b1b217749550
             $manager -> persist($utilisateur);
         }
 
         $manager->flush();
 
-<<<<<<< HEAD
         for($j = 1;$j <= 20; $j++){
             $messages = new Message;
 
@@ -51,41 +36,6 @@ class AppFixtures extends Fixture
             $manager -> persist($messages);            
         }
 
-=======
-        $repoUser = $manager -> getRepository(User::class);
-        $users = $repoUser -> findAll();
-        $nb = count($users);
-
-        
-        for($x = 1; $x <= 20; $x ++){
-
-            $groupe = new Groupe; 
-
-            $groupe -> setNom('Group test N°' . $x);
-            $groupe -> setPhoto('photo_groupe' . $x . '.jpg');
-            $groupe -> setDate(new \DateTime('now'));
-            $groupe -> setUserAdmin($users[0]);
-            $manager -> persist($groupe);
-            
-            for($z = 1; $z <= 4; $z++){
-                $nbUser = rand(1, $nb-1);
-                $groupe -> addUser($users[$nbUser]);
-
-
-                $nbM = rand(1, 10);
-                for($y = 1; $y < $nbM; $y++){
-                   $message = new Message;
-
-                   $message -> setDate(new \DateTime('now'));
-                   $message -> setContent('lorem ipsum...');
-                    $message -> setUser($users[$nbUser]);
-                    $message -> setGroupe($groupe);
-                    $message -> setState(rand(0, 3));
-                    $manager -> persist($message);
-                }
-            }
-        }
->>>>>>> 20440b84651c124632c69b67b400b1b217749550
         $manager->flush();
     }
 }
